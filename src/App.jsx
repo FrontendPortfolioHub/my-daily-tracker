df0f1a4dcb4212d7734d4b635ced6dde1fbe060a
Pavel Maistrenko <maystrenko.p@gmail.com>
2025-01-19 20:13:06 +0100

    implement nav-bar section

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import './App.scss';
 import { Header } from './components/Header';
+import { NavBar } from './components/NavBar/NavBar';
 
 export const App = () => {
   return <div className="App">
     <Header />
+    <NavBar />
   </div>;
 };
---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,12 @@
+export const NavBar = () => {
+  return (
+    <section className="nav-bar container">
+      <ul className="nav-bar__list">
+        <li className="nav-bar__item statistic icon" />
+        <li className="nav-bar__item weight icon" />
+        <li className="nav-bar__item fitnes-center icon" />
+        <li className="nav-bar__item timer icon" />
+      </ul>
+    </section>
+  );
+};
