7efd70d2b84809e36d734b64231678fec8795775
Pavel Maistrenko <maystrenko.p@gmail.com>
2025-01-19 20:28:49 +0100

    added to-do icon in nav bar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -2,6 +2,7 @@ export const NavBar = () => {
   return (
     <section className="nav-bar container">
       <ul className="nav-bar__list">
+        <li className="nav-bar__item to-do-list icon" />
         <li className="nav-bar__item statistic icon" />
         <li className="nav-bar__item weight icon" />
         <li className="nav-bar__item fitnes-center icon" />
